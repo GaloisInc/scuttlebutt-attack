98d1ac18cd41b93f90b7981c587c4135356d5703
Stuart Pernsteiner <spernsteiner@galois.com>
2022-09-09 16:53:11 -0700

    implement MergedChannel for I/O in non-secret programs

---
+++ src/io_merged.rs
@@ -0,0 +1,55 @@
+use core::cell::RefCell;
+use genio::{Read, Write};
+use crate::comm_trace::ThreadState;
+use crate::comm_trace_data;
+
+#[derive(Clone, Copy)]
+pub struct MergedChannel<'a> {
+    state: &'a RefCell<ThreadState>,
+    channel_id: usize,
+}
+
+impl<'a> MergedChannel<'a> {
+    pub fn new(
+        state: &'a RefCell<ThreadState>,
+        channel_id: usize,
+    ) -> MergedChannel<'a> {
+        MergedChannel { state, channel_id }
+    }
+}
+
+impl Read for MergedChannel<'_> {
+    type ReadError = ();
+
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, ()> {
+        let n = self.state.borrow_mut().recv(
+            comm_trace_data::events(),
+            comm_trace_data::data(),
+            self.channel_id,
+            buf,
+        );
+        Ok(n)
+    }
+}
+
+impl Write for MergedChannel<'_> {
+    type WriteError = ();
+    type FlushError = ();
+
+    fn write(&mut self, buf: &[u8]) -> Result<usize, ()> {
+        let n = self.state.borrow_mut().send(
+            comm_trace_data::events(),
+            comm_trace_data::data(),
+            self.channel_id,
+            buf,
+        );
+        Ok(n)
+    }
+
+    fn flush(&mut self) -> Result<(), ()> {
+        Ok(())
+    }
+
+    fn size_hint(&mut self, _bytes: usize) {}
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,5 @@ pub mod server;
 pub mod attacker;
 pub mod comm_trace;
 pub mod comm_trace_data;
+pub mod io_merged;
 pub mod util;
